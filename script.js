4570de464843adaa493cc251591dfc8cf48b88ee
Willi2003 <93141936+Willi2003@users.noreply.github.com>
2022-03-22 00:26:10 +0100

    add JS

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+    let projects = document.querySelector("#projects")
+
+    projects.addEventListener("click", function() {
+        window.scrollTo({top: 1160, behavior: 'smooth'});
+    })   
+
+})
